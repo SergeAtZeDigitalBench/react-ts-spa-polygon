d9f8fc33ab04ada6b701166db4a20124917e34b4
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-03-24 14:37:38 +0000

    feat: installed , setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <div>
-      <h1>Use sync external store</h1>
+      <h1 className="text-3xl font-bold underline text-center">
+        Use sync external store
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
