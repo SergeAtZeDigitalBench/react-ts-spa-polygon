35dd51a2aca8d72d0c1266cc57593b3e2f077f7e
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-03-24 15:09:59 +0000

    feat: provided form context to all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import { useState } from "react";
+import { useState, createContext, useContext } from "react";
+
+type State = Record<"first" | "last", string>;
+type ContextType = [
+  State,
+  React.Dispatch<
+    React.SetStateAction<{
+      first: string;
+      last: string;
+    }>
+  >
+];
+
+const FormContext = createContext<ContextType>([
+  {
+    first: "",
+    last: "",
+  },
+  () => {},
+]);
 
 const TextInput = ({ value }: { value: "first" | "last" }) => {
+  const [store, setStore] = useContext(FormContext);
+
+  const handlEchange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStore((current) => ({ ...current, [value]: event.target.value }));
+  };
+
   return (
     <div className="p-2">
       {value}:{" "}
-      <input className="px-2 py-1 border-2 border-green-400 rounded-md" />
+      <input
+        value={store[value]}
+        onChange={handlEchange}
+        className="px-2 py-1 border-2 border-green-400 rounded-md"
+      />
     </div>
   );
 };
 
 const Display = ({ value }: { value: "first" | "last" }) => {
+  const [store] = useContext(FormContext);
+
   return (
     <div className="p-2">
-      {value}: {""}
+      {value}: {store[value]}
     </div>
   );
 };
@@ -48,13 +79,20 @@ const ContentContainer = () => {
 };
 
 function App() {
+  const [store, setStore] = useState({
+    first: "",
+    last: "",
+  });
+
   return (
-    <div className="mt-2 py-2 px-6 border-2 border-gray-500">
-      <h1 className="text-3xl font-bold underline text-center mt-6 mb-12">
-        Use React Context Optimised
-      </h1>
-      <ContentContainer />
-    </div>
+    <FormContext.Provider value={[store, setStore]}>
+      <div className="mt-2 py-2 px-6 border-2 border-gray-500">
+        <h1 className="text-3xl font-bold underline text-center mt-6 mb-12">
+          Use React Context Optimised
+        </h1>
+        <ContentContainer />
+      </div>
+    </FormContext.Provider>
   );
 }
 
