142d0b6247fafbaa84566a9d0c76097e87cbf86a
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-03-24 11:36:03 +0000

    feat: store initial subscribe implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import store from "./store";
+
+import styles from "./App.module.css";
+
+interface IValueItemProp {
+  item: string;
+}
+
+const useStore = (): [
+  Record<string, number>,
+  (incomingState: Record<string, number>) => void
+] => {
+  const [state, setState] = useState(() => store.getState());
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(setState);
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return [state, store.setState];
+};
+
+const DisplayValue = ({ item }: IValueItemProp) => {
+  const [state] = useStore();
+
+  return (
+    <div>
+      {item}: {state[item]}
+    </div>
+  );
+};
+
+const IncrementValue = ({ item }: IValueItemProp) => {
+  const [state, setState] = useStore();
+
+  const handleClick = () => {
+    setState({ ...state, [item]: state[item] + 1 });
+  };
+
+  return <button onClick={handleClick}>Increment {item}</button>;
+};
+
+const DecrementValue = ({ item }: IValueItemProp) => {
+  const [state, setState] = useStore();
+
+  const handleClick = () => {
+    setState({ ...state, [item]: state[item] - 1 });
+  };
+
+  return <button onClick={handleClick}>Decrement {item}</button>;
+};
 
 function App() {
   return (
-    <div>
-      <h1>Use sync external store</h1>
-    </div>
+    <>
+      <h1 className={styles.AppHeader}>Use sync external store</h1>
+      <div className={styles.App}>
+        <IncrementValue item="value1" />
+        <DisplayValue item="value1" />
+        <DecrementValue item="value2" />
+        <DisplayValue item="value2" />
+      </div>
+    </>
   );
 }
 
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,25 @@
+type SubscriberFn<D> = (value: D) => any;
+
 export const createStore = <S = any>(intialState: S) => {
   let newState = intialState;
+  const subscribers = new Set<SubscriberFn<S>>();
 
   return {
     getState: () => newState,
-    setState: (incomingState: S) => (newState = incomingState),
+    setState: (incomingState: S) => {
+      newState = incomingState;
+      subscribers.forEach((subscriberFn) => {
+        subscriberFn(newState);
+      });
+    },
+    subscribe: (listener: SubscriberFn<S>) => {
+      subscribers.add(listener);
+      return () => subscribers.delete(listener);
+    },
   };
 };
 
-const store = createStore({
+const store = createStore<Record<string, number>>({
   value1: 0,
   value2: 0,
 });
