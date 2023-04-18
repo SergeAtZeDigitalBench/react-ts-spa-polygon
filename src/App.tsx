9affa67e9d5859365e7e33b29ff415d8cd76e6ae
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-04-18 12:06:19 +0100

    fix: title updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">
-        Use sync external store
+        React SPA Polygon
       </h1>
     </div>
   );
