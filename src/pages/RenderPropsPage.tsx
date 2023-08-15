18a17dc6cbcf022fdb41bed05c5e4324d2fbf521
Serge Basangovs <serge.basangovs@bbc.com>
2023-08-15 08:24:33 +0100

    feat, demo, render pros, render function

--- src/pages/RenderPropsPage.tsx
+++ src/pages/RenderPropsPage.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 
 import ProductItem from "../components/ProductItem";
+import CompanyItem from "../components/CompanyItem";
 import List from "../components/ListRenderProps";
-import { products } from "../constants";
-import { IProduct } from "../types";
+import { products, companies } from "../constants";
+import { IProduct, ICompany } from "../types";
 
 const RenderPropsPage = (): JSX.Element => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">Render props</h1>
-      <div className=" max-w-[50%] mx-auto">
-        <List
-          title="Products (render props)"
-          items={products}
-          render={(product: IProduct) => (
-            <ProductItem key={product.productName} product={product} />
-          )}
-        />
+      <div className="flex justify-center gap-2">
+        <div className="flex-1">
+          <List
+            title="Products (render props)"
+            items={products}
+            render={(product: IProduct) => (
+              <ProductItem key={product.productName} product={product} />
+            )}
+          />
+        </div>
+        <div className="flex-1">
+          <List
+            title="Companies (render props)"
+            items={companies}
+            render={(company: ICompany) => (
+              <CompanyItem
+                key={company.companyName}
+                company={company}
+                defaultVisibility={false}
+              />
+            )}
+          />
+        </div>
       </div>
     </div>
   );
