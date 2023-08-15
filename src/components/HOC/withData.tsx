0310a905bfa068bf05b4c4dd66cbce3070edc2b3
Serge Basangovs <serge.basangovs@bbc.com>
2023-08-15 13:44:48 +0100

    feat: hoc pattern

--- src/components/HOC/withData.tsx
+++ src/components/HOC/withData.tsx
@@ -9,81 +9,77 @@ interface State<D = any> {
   error: null | string;
 }
 
-interface IProps {
-  [x: string]: unknown;
+export interface IHocInjectedProps {
+  data: typeof products | null;
+  loading: boolean;
+  error: null | string;
+  handleStartFetch: (errorMessage?: string) => void;
+  handleReset: () => void;
 }
 
-const withData = <D = any, IProps = Record<string, any>>(
-  Component: React.FC<
-    IProps &
-      State<D> & {
-        handleStartFetch: (errorMessage?: string) => void;
-        handleReset: () => void;
-      }
-  >
-) => {
-  const [state, setState] = useState<State<D>>({
-    data: null,
-    loading: false,
-    error: null,
-  });
-  const [isToFetch, setIsToFetch] = useState<boolean>(false);
-  const [fetchingError, setFetchingError] = useState<string | null>(null);
-
-  const handleStartFetch = (errorMessage?: string) => {
-    setIsToFetch(true);
-    errorMessage && setFetchingError(errorMessage);
-  };
-
-  const handleReset = () => {
-    setState({
+const withData = <P extends {}>(
+  Component: React.ComponentType<P & IHocInjectedProps>
+): React.ComponentType<P> => {
+  return (props: P) => {
+    const [state, setState] = useState<State<typeof products>>({
       data: null,
       loading: false,
       error: null,
     });
+    const [isToFetch, setIsToFetch] = useState<boolean>(false);
+    const [fetchingError, setFetchingError] = useState<string | null>(null);
+
+    const handleStartFetch = (errorMessage?: string) => {
+      setIsToFetch(true);
+      errorMessage && setFetchingError(errorMessage);
+    };
+
+    const handleReset = () => {
+      setState({
+        data: null,
+        loading: false,
+        error: null,
+      });
+    };
+
+    useEffect(() => {
+      if (!isToFetch) return;
+      let isMounted = true;
+
+      const fetchData = async () => {
+        setState((current) => ({ ...current, loading: true, error: null }));
+        try {
+          const data = (await mockApiRequest(products, fetchingError)) as any;
+          setState({ data, loading: false, error: null });
+        } catch (err) {
+          const message =
+            err instanceof Error
+              ? err.message
+              : ((err as any).toString() as string);
+          setState({ data: null, loading: false, error: message });
+        } finally {
+          setFetchingError(null);
+          setIsToFetch(false);
+        }
+      };
+      fetchData();
+
+      return () => {
+        isMounted = false;
+      };
+    }, [isToFetch, fetchingError]);
+
+    return (
+      <Component
+        {...props}
+        data={state.data}
+        loading={state.loading}
+        error={state.error}
+        handleReset={handleReset}
+        handleStartFetch={handleStartFetch}
+      />
+    );
   };
-
-  useEffect(() => {
-    if (!isToFetch) return;
-    let isMounted = true;
-
-    const fetchData = async () => {
-      setState((current) => ({ ...current, loading: true, error: null }));
-      try {
-        const data = (await mockApiRequest(products, fetchingError)) as any;
-        setState({ data, loading: false, error: null });
-      } catch (err) {
-        const message =
-          err instanceof Error
-            ? err.message
-            : ((err as any).toString() as string);
-        setState({ data: null, loading: false, error: message });
-      } finally {
-        setFetchingError(null);
-        setIsToFetch(false);
-      }
-    };
-    fetchData();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [isToFetch, fetchingError]);
-
-  return ({
-    ...restProps
-  }: IProps &
-    State<D> & {
-      handleStartFetch: (errorMessage?: string) => void;
-      handleReset: () => void;
-    }) => (
-    <Component
-      {...restProps}
-      {...state}
-      handleReset={handleReset}
-      handleStartFetch={handleStartFetch}
-    />
-  );
 };
 
 export default withData;
--- src/pages/HocPage.tsx
+++ src/pages/HocPage.tsx
@@ -1,11 +1,70 @@
 import React from "react";
 
+import withData, { IHocInjectedProps } from "../components/HOC/withData";
+import List from "../components/List";
+
+interface IPropsOriginal {
+  title: string;
+}
+
+const WrappedComponent = withData(
+  ({
+    title,
+    data,
+    loading,
+    error,
+    handleStartFetch,
+    handleReset,
+  }: IPropsOriginal & IHocInjectedProps) => {
+    return (
+      <div>
+        <div className="flex gap-4 my-4">
+          <button
+            onClick={() => handleStartFetch()}
+            className="px-4 py-2 bg-green-500 text-white"
+          >
+            fetch data with success
+          </button>
+          <button
+            onClick={() => handleStartFetch("Fetching error message")}
+            className="px-4 py-2 bg-red-600 text-white"
+          >
+            fetch data with error
+          </button>
+
+          <button
+            onClick={handleReset}
+            className="px-4 py-2 bg-gray-600 text-white"
+          >
+            reset
+          </button>
+        </div>
+        <div>
+          {loading && (
+            <p className="p-4 bg-blue-700 text-white text-center">Loading...</p>
+          )}
+          {error && (
+            <p className="p-4 bg-red-600 text-white text-center">{error}</p>
+          )}
+          {!loading && !error && data && <List title={title} items={data} />}
+        </div>
+      </div>
+    );
+  }
+);
+
 interface IProps {
   [x: string]: unknown;
 }
 
 const HocPage = ({}: IProps): JSX.Element => {
-  return <div>HocPage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">Render props</h1>
+
+      <WrappedComponent title="Products (via HOC)" />
+    </div>
+  );
 };
 
 export default HocPage;
