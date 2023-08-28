0c39c3406599bdb8480a2aa0fd3ad8751a427b39
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-08-28 11:38:57 +0300

    feat: page heading updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">
-        Use sync external store
+        React TS SPA Polygon
       </h1>
     </div>
   );
